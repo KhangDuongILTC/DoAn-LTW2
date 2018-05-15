a9b420732b1f61ff470c577bfe99b0de4ae5fda7
boylili023 <32019966+boylili023@users.noreply.github.com>
2018-05-16 00:45:18 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,77 +1,23 @@
-import * as Types from './../constants/ActionTypes';
-import callApi from './../utils/apiCaller';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import appReducers from './reducers/index';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
-export const actFetchProductsRequest = () => {
-    return dispatch => {
-        return callApi('products', 'GET', null).then(res => {
-            dispatch(actFetchProducts(res.data));
-        });
-    };
-}
+const store = createStore(
+    appReducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(thunk)
+);
 
-export const actFetchProducts = (products) => {
-    return {
-        type : Types.FETCH_PRODUCTS,
-        products
-    }
-}
-
-export const actDeleteProductRequest = (id) => {
-    return dispatch => {
-        return callApi(`products/${id}`, 'DELETE', null).then(res =>{
-            dispatch(actDeleteProduct(id));
-        })
-    }
-}
-
-export const actDeleteProduct = (id) => {
-    return {
-        type : Types.DELETE_PRODUCT,
-        id
-    }
-}
-
-export const actAddProductRequest = (product) => {
-    return dispatch => {
-        return callApi('products', 'POST', product).then(res => {
-            dispatch(actAddProduct(res.data));
-        });
-    }
-}
-
-export const actAddProduct = (product) => {
-    return {
-        type : Types.ADD_PRODUCT,
-        product
-    }
-}
-
-export const actGetProductRequest = (id) => {
-    return dispatch => {
-        return callApi(`products/${id}`, 'GET', null).then(res => {
-            dispatch(actGetProduct(res.data));
-        });
-    }
-}
-
-export const actGetProduct = (product) => {
-    return {
-        type : Types.EDIT_PRODUCT,
-        product
-    }
-}
-
-export const actUpdateProductRequest = (product) => {
-    return dispatch => {
-        return callApi(`products/${product.id}`, 'PUT', product).then(res => {
-            dispatch(actUpdateProduct(res.data));
-        });
-    }
-}
-
-export const actUpdateProduct = (product) => {
-    return {
-        type : Types.UPDATE_PRODUCT,
-        product
-    }
-}
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root')
+);
+registerServiceWorker();
